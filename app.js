3d3f3bae8adcaad2b7ee5eb2d4ce8107d036d43d
Alexander Byun <alexanderbyun@MacBook-Pro.attlocal.net>
2022-05-03 06:03:50 -0400

    resume page complete. 3rd interaction complete

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 $(() => {
 
 // =================================================
+// About
+// =================================================
+
+let currentImgIndex = 0
+let numOfImages = $('.carousel-images').children().length-1
+
+$('.next').on('click', () => {
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+        if (currentImgIndex < numOfImages) {
+        currentImgIndex++
+        } else {
+        currentImgIndex = 0
+        }
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+  })
+
+  $('.prev').on('click', () => {
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+        if (currentImgIndex > 0) {
+        currentImgIndex--
+        } else {
+        currentImgIndex = numOfImages
+        }
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+  })
+
+    
+// =================================================
 // Projects
 // =================================================
 
