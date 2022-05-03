7eec9a84bf49e5de8dc99108081fe8d90bae5595
Alexander Byun <alexanderbyun@MacBook-Pro.local>
2022-05-03 04:04:08 -0400

    webpages all done, 2 interactions done

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
 $(() => {
 
+// =================================================
+// Projects
+// =================================================
+
+    const $openBtn1 = $('#openModal1')
+    const $openBtn2 = $('#openModal2')
+    const $closeBtn1= $('#close1')
+    const $closeBtn2= $('#close2')
+
+    const $modal1 = $('#modal1')
+    const $modal2 = $('#modal2')
+
+    const openModal1 = () => {
+        $modal1.css('display', 'block')
+    }
+    const openModal2 = () => {
+        $modal2.css('display', 'block')
+    }
+
+    const closeModal1 = () => {
+        $modal1.css('display', 'none')
+    }
+    const closeModal2 = () => {
+        $modal2.css('display', 'none')
+    }
+    
+    $openBtn1.on('click', openModal1)
+    $closeBtn1.on('click', closeModal1)
+    $openBtn2.on('click', openModal2)
+    $closeBtn2.on('click', closeModal2)
+
+// =================================================
+// Footer
+// =================================================
+
 const $footer = $('<div>').attr('class', 'flex-footer')
 
 const $githubImg = $('<img>').attr('src', 'images/github-blk.png').width(50)
@@ -13,6 +48,10 @@ const $facebookUrl = $('<a>').attr('href', 'http://facebook.com').attr('target',
 const $instagramUrl = $('<a>').attr('href', 'http://instagram.com').attr('target', 'blank')
 const $linkedinUrl = $('<a>').attr('href', 'http://linkedin.com').attr('target', 'blank')
 const $twitterUrl = $('<a>').attr('href', 'http://twitter.com').attr('target', 'blank').css('transition', '0.6s', 'ease-in-out')
+
+    // Searched for how to make an image a hyperlink in jQuery and learned about wraps. Following resources used: 
+    // https://api.jquery.com/wrap/
+    // https://weblogs.asp.net/christoc/adding-a-hyperlink-to-an-image-img-with-jquery
 
 $('.flex-footer-cont').append($footer)
 $githubImg.appendTo($footer).wrap($githubUrl)
